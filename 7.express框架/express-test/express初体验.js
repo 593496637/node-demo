8ab7ad277c4871cbba62f877abbf4d84ddf7a771
likaikai <593496637@qq.com>
2021-06-23 00:20:35 +0800

    express

---
+++ 7.express框架/express-test/express初体验.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+// 创建服务器
+const app = express()
+
+// 监听默认路径
+app.get('/', (req, res, next) => {
+  res.end('hello express')
+})
+
+app.post('/', (req, res, next) => {
+  res.end('hello post express')
+})
+
+app.post('/login', (req, res, next) => {
+  res.end('welcome back')
+})
+
+app.listen(8000, err => {
+  console.log('服务已启动');
+})
